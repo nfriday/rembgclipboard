097effd6cea32ad7a6d443be4f7806779b81fd81
Nick Friday <nickjfriday@gmail.com>
2025-11-30 12:53:59 +0000

    don't use poetry2nix

--- flake.nix
+++ flake.nix
@@ -1,21 +1,28 @@
 {
   description = "rembgclipboard";
   
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    poetry2nix.url = "github:nix-community/poetry2nix";
-    poetry2nix.inputs.nixpkgs.follows = "nixpkgs";
-  };
+  inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
 
-  outputs = { self, nixpkgs, poetry2nix }:
+  outputs = { self, nixpkgs }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
-      inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryApplication;
+      python-with-packages = pkgs.python3.withPackages (ps: [ 
+        ps.pillow 
+        ps.rembg 
+      ]);
     in
     {
-      packages.${system}.default = mkPoetryApplication {
-        projectDir = ./.;
+      packages.${system}.default = pkgs.writeShellApplication {
+        name = "rembgclipboard";
+        runtimeInputs = [ 
+          python-with-packages
+          pkgs.wl-clipboard
+          pkgs.libnotify
+        ];
+        text = ''
+          python ${self}/src/rembgclipboard/app.py "$@"
+        '';
       };
     };
 }
